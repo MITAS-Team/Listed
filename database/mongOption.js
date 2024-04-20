34f45f610525631db99e4709b8d397310f5a98bb
laiso <aloisleroy2016@gmail.com>
2024-04-20 16:31:48 +0200

    adding connection handling

--- database/mongOption.js
+++ database/mongOption.js
@@ -4,8 +4,20 @@ module.exports = {
     init: (connect) => {
         mongoose.connect(connect);
         mongoose.Promise = global.Promise;
+
+        // Send console message if connected to mongoDB
         mongoose.connection.on("connected", () => {
-            console.log("Successfuly connected to mongoDB");
+            console.log("[mongoDB]: connected");
+        });
+
+        // Send console message if mongoDB is disconected
+        mongoose.connection.on("disconnected",() => {
+            console.log("[mongoDB]: disconnected");
+        });
+
+        // Send error message if connection 
+        mongoose.connection.on("error", (error) => {
+            console.log("[mongoDB]: An error occured", error);
         });
     },
 };
