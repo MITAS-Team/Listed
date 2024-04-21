9b16edcdc3447cb328851c44445abf06e5589dae
laiso <aloisleroy2016@gmail.com>
2024-04-21 22:16:02 +0200

    Add presence status and activity for the client

--- events/ready.js
+++ events/ready.js
@@ -5,5 +5,12 @@ module.exports = {
 	once: true,
 	async execute(client) {
 		console.log(`\x1b[92m[events]: ${client.user.tag} is up\x1b[0m`);
+		client.user.setPresence({
+			activities: [{
+				name: "With commands",
+				type: 'PLAYING'
+			}],
+			status: 'online'
+		});
 	},
 };
