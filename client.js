542e094fa3e42e66e66e5a6db49e6a486482f9b7
laiso <aloisleroy2016@gmail.com>
2024-04-30 21:02:46 +0200

    Fix DEBUG_MODE variable assignment in client.js

--- client.js
+++ client.js
@@ -1,12 +1,11 @@
 // import env variable
 require('dotenv').config();
 const { token, connect } = process.env;
-const DEBUG_MODE = true;
 
 /*
     Creating the client
 */
-const { Client, Collection, GatewayIntentBits, } = require('discord.js');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({ 
     intents: [
@@ -18,17 +17,28 @@ const client = new Client({
 });
 
 client.commands = new Collection();
-
-// Initialize the connection to mongoDB
+/*
+    Connect to the database
+    @param {String} connect - The mongodb connection URI
+*/
 require('./database/mongOption.js').init(connect);
 
 // Deploy the commands
 require('./handler/deploy-commands.js').init();
 
-// Initialize the commands and events handlers
+/*
+    Initialize the commands and events handlers
+    @param {Client} client - The discord client
+    @param {Boolean} DEBUG_MODE - Print events and commands loaded
+*/
+const DEBUG_MODE = true; // Print events and commands loaded
 require('./handler/commands.js').init(client, DEBUG_MODE);
 require('./handler/events.js').init(client, DEBUG_MODE);
 
 
-// Login the client
-client.login(token);
+try {
+    // Login the client
+    client.login(token);
+} catch (error) {
+    console.error('[client]: Error logging in:', error);
+}
