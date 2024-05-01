26bbb296f4386b3ca8682fe6a2bfd91bd697e208
laiso <aloisleroy2016@gmail.com>
2024-05-01 18:29:16 +0200

    Refactor ready event to set bot's presence in events/ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,16 +1,22 @@
-const { Events } = require('discord.js');
+const { Events, ActivityType, PresenceUpdateStatus } = require('discord.js');
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	async execute(client) {
 		console.log(`\x1b[92m[events]: ${client.user.tag} is up\x1b[0m`);
+
+		// Set the bot's presence
 		client.user.setPresence({
-			activities: [{
-				name: "With commands",
-				type: 'PLAYING'
-			}],
-			status: 'online'
+			// Set the status to online
+			status: PresenceUpdateStatus.Online,
+			// Set the activity to listening to /help
+			activities: [
+				{
+					name: '/help',
+					type: ActivityType.LISTENING,
+				},
+			],
 		});
 	},
 };
